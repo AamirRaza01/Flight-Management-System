4fc2034b3d7200f4c5e783a9bd9afa118ea49a69
Don Basumatary <127093790+donbasu15@users.noreply.github.com>
2024-06-01 06:51:07 +0530

    Update searchflight.js
    search feature returning array of possible routes of travelling

--- src/controllers/searchflight.js
+++ src/controllers/searchflight.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utilities/apiError.js";
 
 const Search = asyncHandler(async (req, res) => {
     let { from, to, date } = req.body;
-    console.log(req.body);
+   
     let d = new Date(date);
     const startOfDay = new Date(d.setHours(0, 0, 0, 0));
     
@@ -26,8 +26,7 @@ const Search = asyncHandler(async (req, res) => {
         
         const endOfDay = new Date(fly);
         endOfDay.setDate(endOfDay.getDate() + 1);
-       console.log(startOfDay);
-       console.log(endOfDay);
+       
        let af = await Flight.find({
             departureCity: fromCity,
             departureDateTime: { $gte: startOfDay, $lt: endOfDay }
@@ -35,14 +34,12 @@ const Search = asyncHandler(async (req, res) => {
         
         for (let a of af) {
             console.log("hello");
-            console.log(level);
-            console.log(result);
-            console.log(current);
+            
             if (a.arrivalCity === toCity) {
                 current.push(a.arrivalCity);
-                console.log(current);
+                
                 result.push([...current]);
-                console.log("pushed in result");
+               
                 current.pop();
                 current.pop();
                 return;
@@ -55,7 +52,7 @@ const Search = asyncHandler(async (req, res) => {
         current.pop();
     }
     
-    console.log(from, to);
+    
     await findFlight(from, to, 0, startOfDay,startOfDay);
     console.log(result);
     if (!result.length) {
